debe696913bdc48855a9846381ba3fa4f814e565
Tyler Encke <tylerencke@gmail.com>
2019-10-09 23:02:32 -0400

    Update packages and

--- packages/common/src/utils/array.util.spec.ts
+++ packages/common/src/utils/array.util.spec.ts
@@ -1,7 +1,14 @@
 import { ArrayUtil } from './array.util';
 
 describe('ArrayUtil', () => {
-  it('should be defined', () => {
-    expect(new ArrayUtil()).toBeDefined();
+  it('isEmpty should return true on empty', () => {
+    expect(ArrayUtil.isEmpty([])).toBeTruthy();
+    expect(ArrayUtil.isEmpty(null)).toBeTruthy();
+    expect(ArrayUtil.isEmpty(undefined)).toBeTruthy();
+  });
+
+  it('isEmpty should return false on not empty', () => {
+    expect(ArrayUtil.isEmpty([''])).toBeFalsy();
+    expect(ArrayUtil.isEmpty(['a'])).toBeFalsy();
   });
 });
